8a4252988a4149b247a3455ec01be548fd414442
OgarMikeDev <123175953+OgarMikeDev@users.noreply.github.com>
2024-09-20 13:14:19 +0300

    Update Main.java

--- src/main/java/org/example/binarySearch/Main.java
+++ src/main/java/org/example/binarySearch/Main.java
@@ -1,24 +1,25 @@
 package org.example.binarySearch;
 
 public class Main {
-    public static void main(String[] args) throws java.lang.Exception {
+public static void main(String[] args) throws java.lang.Exception {
         // Your code goes here
-        int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+                       //А  Б  В  Г  Д  Е  Ё  Ж  З  И
+        int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
         int start = 0;
         int end = numbers.length - 1;
-        int middle = end / 2;
-        int query = 8;
+        int middle = (start + end) / 2;
+        int query = 6;
 
         while (true) {
+            middle = (start + end) / 2;
+            System.out.println("Middle: " + middle);
             if (query == numbers[middle]) {
                 System.out.println("Element " + numbers[middle] + " found!");
                 break;
             } else if (query < numbers[middle]) {
                 end = middle - 1;
-                middle = start + (end - start) / 2;
             } else if (query > numbers[middle]) {
                 start = middle + 1;
-                middle = start + (end - start) / 2;
             }
         }
     }
